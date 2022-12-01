1f5340c51a552b62d0564367bd6b60f8385cbe81
infstka <malder100@tut.by>
2022-12-01 23:14:36 +0300

    modified LR10

--- LR10/src/test/java/Test/MusicMarketSecondTest.java
+++ LR10/src/test/java/Test/MusicMarketSecondTest.java
@@ -30,20 +30,22 @@ public class MusicMarketSecondTest {
     }
 
     @Test
-    public void TestSort() {
+    public void TestSort() throws InterruptedException {
         var MainPage = new MainPage(driver);
         MainPage.OpenPageFilter();
         MainPage.SortItems();
+        Thread.sleep(2000);
         driver.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div/div[3]/div/div[1]/div/div[2]/div/div[3]/div[1]/div[1]/div/form/div/div[2]/div[1]/a")).click();
     }
 
     @Test
-    public void AllTogether() {
+    public void AllTogether() throws InterruptedException {
         var MainPage = new MainPage(driver);
         MainPage.OpenPage();
         MainPage.SearchFor("Струны для электрогитары");
         MainPage.SortItems();
-       //driver.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div/div[3]/div/div[1]/div/div[2]/div/div[3]/div[1]/div[1]/div/form/div/div[2]/div[1]/a")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("/html/body/div[1]/div[4]/div[3]/div/div[3]/div/div[1]/div/div[2]/div/div[3]/div[1]/div[1]/div/form/div/div[2]/div[1]/a")).click();
     }
 
     @AfterEach
